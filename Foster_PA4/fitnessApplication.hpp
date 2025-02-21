33d26a53fab6ae44deaf299535a2757e90fc5f52
Isaiah Foster <isaiahfoster@gmail.com>
2025-02-20 22:38:46 -0800

    PC Work 2/19

--- Foster_PA4/fitnessApplication.hpp
+++ Foster_PA4/fitnessApplication.hpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "ExercisePlan.hpp"
+#include "DietPlan.hpp"
 
 using std::string;
 using std::fstream;
@@ -10,3 +12,24 @@ using std::cin;
 using std::endl;
 using std::ifstream;
 using std::ostream;
+
+class FitnessAppWrapper
+{
+private:
+	DietPlan* dietPlansHead;
+	ExercisePlan* exercisePlansHead;
+
+	template <typename T>
+	void loadDailyPlan(ifstream& filesStream, T* plan);
+
+	template <typename T>
+	void loadWeeklyPlan(ifstream& filesStream, T* plan);
+
+	template <typename T>
+	void displayDailyPlan(T* plan);
+
+	template <typename T>
+	void displayWeeklyPlan(T* plan);
+
+
+	
